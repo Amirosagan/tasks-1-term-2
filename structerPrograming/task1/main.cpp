f5d1711b2a8f9134dcb91d1ac136e1ff476f3786
Amir <sagan.5050@gmail.com>
2022-03-17 20:06:22 +0200

    task1.01

--- structerPrograming/task1/main.cpp
+++ structerPrograming/task1/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main () {
-    
+
     long double aSide {0} , bSide {0},cSide {0} ;
     cout << "Entre your triangle (a) side : \n -> ";
     cin >> aSide;
@@ -16,6 +16,10 @@ int main () {
     cout << "perimeter of triangle = " << aSide+bSide+cSide << endl;
 
     cout << "area of your triangle = " << 0.25*pow((aSide+bSide-cSide) * (aSide-bSide+cSide) * (-aSide+bSide+cSide) * (aSide+bSide+cSide),0.5) << endl;
+
+    system("pause");
+
+    return 0;
 
 }
 
