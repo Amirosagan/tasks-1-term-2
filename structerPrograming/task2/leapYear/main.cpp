6899ad2bfdf749ced32604396450d2104667bd98
Amir <sagan.5050@gmail.com>
2022-04-02 04:20:23 +0200

    task2

---
+++ structerPrograming/task2/leapYear/main.cpp
@@ -0,0 +1,17 @@
+#include <bits\stdc++.h>
+
+using namespace std;
+
+int main () {
+    int year ;
+    cout << "please entre year \n -> ";
+    cin >> year;
+    if (year %400 ==0|| (year % 4 ==0 && year % 100 != 0)) 
+        cout <<year <<  " is leap year !!!!\n";
+    else 
+        cout << year << " not leap year.\n" ;
+
+
+        return 0;
+    
+}
